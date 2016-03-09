fbaaa825fe106ee1aa0fd57feaf0e8708c97571c
louisguitton <louisguitton93@gmail.com>
2016-03-09 21:39:29 +0100

    plot

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 f = h5py.File("data/Test_Louis_Test_Louis_gauche_04-mars2016.h5", "r")
 
@@ -10,3 +12,11 @@ dset = f['signal_0/sig']
 print dset
 print dset.shape
 # print dset.type
+plt.plot(dset)
+plt.ylabel('Louis Gauche')
+plt.show()
+
+# frequence d'echantillonage en Hz
+freq = 250
+length_acquisition = dset.shape / (freq*60)  # in minutes
+print length_acquisition
