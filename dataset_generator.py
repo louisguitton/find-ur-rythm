b485abe024015d7b9aed46597f2e1a4432658955
CharlesMasson <charles.masson.28@gmail.com>
2016-03-16 17:34:29 +0100

    Added crop_sample and cut_sample functions

--- dataset_generator.py
+++ dataset_generator.py
@@ -28,13 +28,13 @@ playlist1 = {
     'rythmic': [1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
     'charles likes': [0, 0, 0, 0, 1, 0, 0, 1, 1, 0],
     'louis likes': [1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
-    }
+}
 playlist2 = {
     'sung': [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
     'rythmic': [0, 1, 0, 1, 0, 1, 1, 1, 1, 1],
     'charles likes': [1, 1, 0, 0, 0, 0, 0, 1, 0, 0],
     'louis likes': [1, 0, 1, 0, 1, 1, 1, 0, 0, 0]
-    }
+}
 
 features1 = [None] * 10
 features2 = [None] * 10
@@ -44,7 +44,7 @@ for i in range(10):
         'rythmic': playlist1["rythmic"][i],
         'charles likes': playlist1["charles likes"][i],
         'louis likes': playlist1["louis likes"][i]
-        }
+    }
     features2[i] = {
         'sung': playlist2["sung"][i],
         'rythmic': playlist2["rythmic"][i],
@@ -69,44 +69,22 @@ def get_array(dataD, dataG, start_enr):
         }
     return(enr)
 
+
+def crop_sample(sample, start_time, end_time):
+    cropped_sample = {}
+    for signal in sample:
+        cropped_sample[signal] = sample[signal][int(start_time * FREQ):int(end_time * FREQ)]
+    return(cropped_sample)
+
+
+def cut_sample(sample, length):
+    samples = []
+    for i in range(int(len(sample['T8']) / (FREQ * length))):
+        samples.append(crop_sample(sample, i * length, (i + 1) * length))
+    return(samples)
+
+
 enrL1 = get_array(h5dataLD, h5dataLG, START_L1)
 enrL2 = get_array(h5dataLD, h5dataLG, START_L2)
 enrC1 = get_array(h5dataCD, h5dataCG, START_C1)
 enrC2 = get_array(h5dataCD, h5dataCG, START_C2)
-
-
-# def cut_sample(enr, start, end):
-#     return({
-#         'T8': enr['T8'][start:end],
-#         'C4': enr['C4'][start:end],
-#         'F4': enr['F4'][start:end],
-#         'E2': enr['E2'][start:end],
-#         'T7': enr['T7'][start:end],
-#         'C3': enr['C3'][start:end],
-#         'CZ': enr['CZ'][start:end]
-#     })
-
-def get_input_sample(enr, start, end):
-    sample = []
-    sample.extend(abs(numpy.fft.fft(enr['T8'][start:end])))
-    return(sample)
-
-
-
-def add_samples(x, y, enr, features, length):
-    for i in range(len(enr)):
-        for j in range(int(len(enr[i]['T8']) / (FREQ * length))):
-            # x.append(enr[i]['C4'][j * FREQ * length:(j + 1) * FREQ * length])
-            x.append(get_input_sample(enr[i], j * FREQ * length, (j + 1) * FREQ * length))
-            # x.append(cut_sample(enr[i], j * FREQ * length, (j + 1) * FREQ * length - 1))
-            y.append(features1[i]['sung'])
-
-# print(abs(numpy.fft.fft(enrL1[0]['T8'][0:20])))
-print(get_input_sample(enrL1[0], 0, 1500))
-
-
-
-
-# def reduce_array(x):
-
-# print(len(abs(numpy.fft.fft(enrL1[0]['T8'][0:20]))))
