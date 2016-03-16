f9df96b3cbb079af1481d180f8382db576413f24
louisguitton <louisguitton93@gmail.com>
2016-03-16 20:39:15 +0100

    unsupervised analysis with pictures

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-from dataset_generator import *
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
-import numpy as np
-
-h5dataLD = h5py.File("data/Test_Louis_droite_Test_Louis_droite_04-mars2016.h5", "r")
 
 
-# plt.plot(h5dataLD['signal_3/sig'])
-plt.plot(enrL2[0]["E2"])
+plt.plot([1, 30, 40])
 plt.show()
--- unsupervised.py
+++ unsupervised.py
@@ -1,8 +1,102 @@
 # Est-ce que quand c’est chanté le préfrontal est plus activé?
 from dataset_generator import *
+import matplotlib
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
 import numpy as np
+import spectrum
+
+
+FREQ_RANGES = [[0.5, 4], [4, 8], [8, 12], [11, 15], [15, 30], [30, 45]]
+labels = ["Delta", "Theta", "Alpha1", "Alpha2", "Beta", "Gamma"]
+
+SIGNALS_TO_ANALYZE = ["T8", "C4", "F4", "T7", "C3", "CZ"]
+
+
+def get_spectral_intensities(data, ranges=FREQ_RANGES):
+    intensities = []
+    spectral_data = spectrum.pmtm(data, 2.5, show=False)
+    l = len(data)
+    for [min, max] in ranges:
+        intensities.extend(sum(spectral_data[int(min * l / FREQ):int(max * l / FREQ)]))
+    return intensities
+
+
+def get_all_spectral_intensities(sample, ranges=FREQ_RANGES, signals=SIGNALS_TO_ANALYZE):
+    intensities = []
+    for signal in signals:
+        intensities.extend(get_spectral_intensities(sample[signal], ranges))
+    return(intensities)
+
+plt.figure("C2['F4']")
+width = 1
+# Sung
+plt.subplot(521)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[0]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Nekfeu")
+
+plt.subplot(523)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[2]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Patrick Sebastien")
+
+plt.subplot(525)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[3]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Gorillaz")
+
+plt.subplot(527)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[4]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Above n Beyond")
+
+plt.subplot(529)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[9]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Glaciation")
+
+# Instru
+plt.subplot(522)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[1]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("District7")
+
+plt.subplot(524)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[5]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Victor Wooten")
+
+plt.subplot(526)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[6]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Stravinsky")
+
+plt.subplot(528)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[7]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("Symbolic n Avalon")
+
+plt.subplot(520)
+plt.bar(np.arange(6), get_spectral_intensities(enrC2[8]["F4"]))
+plt.xticks(np.arange(6) + width/2., labels)
+plt.yscale('log')
+plt.ylabel("City Lies")
+
+plt.show()
+# plt.savefig("C2.png")
+
+
+
 
 # prefrontal_sung = np.array([])
 # prefrontal_instru = np.array([])
@@ -16,64 +110,19 @@ import numpy as np
 # prefrontal_sung = abs(fft(prefrontal_sung))
 # prefrontal_instru = abs(fft(prefrontal_instru))
 
-
-def ffty(y):
-    # len(y)=T*N
-    # de 0 a 40 Hz
-    fmax = 40.0
-    T = 1.0 / (2.0 * fmax)
-    # Number of samplepoints
-    N = len(y)/T
-    print("N ", N)
-    yf = fft(y)
-    print(len(yf))
-    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-    print(len(xf))
-    plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
-    plt.grid()
-    plt.show()
-
-
-plt.figure(1)
-# Sung
-plt.subplot(521)
-ffty(enrL1[0]["F4"])
-plt.ylabel("Nekfeu")
-
-plt.subplot(523)
-ffty(enrL1[2]["F4"])
-plt.ylabel("Patrick Sebastien")
-
-plt.subplot(525)
-ffty(enrL1[3]["F4"])
-plt.ylabel("Gorillaz")
-
-plt.subplot(527)
-ffty(enrL1[4]["F4"])
-plt.ylabel("Above n Beyond")
-
-plt.subplot(529)
-ffty(enrL1[9]["F4"])
-plt.ylabel("Glaciation")
-# Instru
-plt.subplot(522)
-ffty(enrL1[1]["F4"])
-plt.ylabel("District7")
-
-plt.subplot(524)
-ffty(enrL1[5]["F4"])
-plt.ylabel("Victor Wooten")
-
-plt.subplot(526)
-ffty(enrL1[6]["F4"])
-plt.ylabel("Stravinsky")
-
-plt.subplot(528)
-ffty(enrL1[7]["F4"])
-plt.ylabel("Symbolic n Avalon")
-
-plt.subplot(520)
-ffty(enrL1[8]["F4"])
-plt.ylabel("City Lies")
-
-plt.show()
+#
+# def ffty(y):
+#     # len(y)=T*N
+#     # de 0 a 40 Hz
+#     fmax = 40.0
+#     T = 1.0 / (2.0 * fmax)
+#     # Number of samplepoints
+#     N = len(y)/T
+#     print("N ", N)
+#     yf = fft(y)
+#     print(len(yf))
+#     xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+#     print(len(xf))
+#     plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
+#     plt.grid()
+#     plt.show()
